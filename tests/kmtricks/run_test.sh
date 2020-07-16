ea1eb7e64ba94a01cf562513afae088bb16db12f
Teo Lemane <teo.lemane@gmail.com>
2020-07-16 11:34:58 +0200

    ctest

--- tests/kmtricks/run_test.sh
+++ tests/kmtricks/run_test.sh
@@ -16,6 +16,7 @@ kmersize=20
 ${binary} -file ${fof} -run-dir ./run_test -kmer-size ${kmersize} -matrix-fmt 4 -nb-cores 1 -nb-parts ${nbpart} -max-memory 1000 -keep-tmp 1 -max-hash 100 -abundance-min 1 -recurrence-min 1 #-only 4
 
 #check superkmers partitions
+echo -ne "Superkmers partitions ..."
 while IFS= read -r file
 do
     for (( i=0; i<${nbpart}; i++ ))
@@ -25,8 +26,10 @@ do
         check_exit_code
     done
 done < ${fof}
+echo -e "\rSuperkmers partitions ... ok"
 
 #check hashes partitions
+echo -ne "Hash/Kmer partitions ..."
 for (( i=0; i<${nbpart}; i++ ))
 do
     while IFS= read -r file
@@ -36,8 +39,10 @@ do
         check_exit_code
     done < ${fof}
 done
-   
+echo -e "\rHash/Kmer partitions ... ok"
+
 #check merge and trp
+echo -ne "Merge/Tranpose ..."
 for (( i=0; i<${nbpart}; i++ ))
 do
     diff ./res/matrices/partition_${i}/no_trp_bf${i}.mat ./run_test/storage/matrix/partition_${i}/no_trp_bf${i}.mat
@@ -45,5 +50,6 @@ do
     diff ./res/matrices/partition_${i}/trp_bf${i}.mat ./run_test/storage/matrix/partition_${i}/trp_bf${i}.mat
     check_exit_code
 done
+echo -e "\rMerge/Transpose ... ok"
 
 rm -rf ./run_test
