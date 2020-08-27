02df1681dd7a8352ee1be6dfc0d41dba27c91780
Teo Lemane <teo.lemane@gmail.com>
2020-08-27 12:04:02 +0200

    fix path

--- tests/libs/run_test.sh
+++ tests/libs/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-../../libs/snippets/merger_ex ./merge_data/fof.txt 1 1 > tmp11.txt
-../../libs/snippets/merger_ex ./merge_data/fof.txt 1 2 > tmp12.txt
-../../libs/snippets/merger_ex ./merge_data/fof.txt 2 1 > tmp21.txt
+../../bin/snippets/merger_ex ./merge_data/fof.txt 1 1 > tmp11.txt
+../../bin/snippets/merger_ex ./merge_data/fof.txt 1 2 > tmp12.txt
+../../bin/snippets/merger_ex ./merge_data/fof.txt 2 1 > tmp21.txt
 
 diff ./merge_data/merge_a1r1.txt tmp11.txt
 if [ $? -ne 0 ]; then
@@ -21,8 +21,8 @@ fi
 
 rm tmp11.txt tmp12.txt tmp21.txt
 
-../../libs/snippets/bitmatrix_ex > /dev/null
+../../bin/snippets/bitmatrix_ex > /dev/null
 
-../../libs/snippets/skreader_ex ./skreader_data/sk_part > tmpSk0.txt
+../../bin/snippets/skreader_ex ./skreader_data/sk_part > tmpSk0.txt
 diff ./skreader_data/sk_part/superks0.txt tmpSk0.txt
 rm tmpSk0.txt
