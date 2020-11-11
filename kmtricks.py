78a1872921e08647f4166e91a4cf4a9b8476bad5
Teo Lemane <teo.lemane@gmail.com>
2020-11-11 18:12:06 +0100

    add: license

--- kmtricks.py
+++ kmtricks.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   kmtricks
+#   Authors: T. Lemane
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 import sys
 import os
 import abc
@@ -17,7 +36,7 @@ from types import FunctionType
 from shutil import rmtree
 from copy import deepcopy, copy
 
-__version__ = '0.0.1'
+__version__ = '0.0.0'
 
 MIN_PYTHON = (3, 5)
 if sys.version_info < MIN_PYTHON:
