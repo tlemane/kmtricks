a5401305134590877fcb0ae15a527f9399e4caef
Teo Lemane <teo.lemane@gmail.com>
2021-01-23 23:29:36 +0100

    clean

--- scripts/auto_release.py
+++ scripts/auto_release.py
@@ -16,43 +16,14 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# Auto release from Travis CI for kmtricks
-# 
-# Require in .travis.yml:
-# 
-#       python: "3.6"
-# 
-#       addons:
-#           apt:
-#               packages:
-#                   - "python3"
-#                   - "python3-setuptools"
-#                   - "python3-pip"
-#       script:
-#           - cmake .. -DMAKE_RELEASE=1 && make
-#           - make package
-#           - python3 ./scripts/auto_release.py
-#
-# New release if:
-#   -  __version__ in kmtricks.py > latest release
-#   -  no release and __version__ > 0.0.0
-#
-# Also __version__ in kmtricks.py and project(kmtricks VERSION 0.0.1 in 
-# main CMakeLists.txt must be equal.
-#
-# Description file : kmtricks/doc/releases/desc-X.X.X.txt
-#
-#
-
 from github import Github
 import os
 import sys
 import platform
-import importlib
+from kmtricks import __version__ as kmtricks_version
 
 py_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(py_path+'/..')
-from kmtricks import __version__ as kmtricks_version
 
 system = platform.system()
 
@@ -63,7 +34,7 @@ PACKAGE_PATH = f'{py_path}/../build/{PACKAGE_NAME}'
 try:
     d = sys.argv[1]
     PACKAGE_PATH = f'{d}/{PACKAGE_NAME}'
-except:
+except IndexError:
     pass
 
 PACKAGE_EXISTS = os.path.exists(PACKAGE_PATH)
@@ -100,10 +71,13 @@ print(PACKAGE_EXISTS)
 print(PACKAGE_PATH)
 print(LATEST_VERSION, CURRENT_VERSION)
 
-if not CURRENT_VERSION == [0,0,0]:
+if not CURRENT_VERSION == [0, 0, 0]:
     if CURRENT_VERSION > LATEST_VERSION or not LATEST_VERSION:
         if PACKAGE_EXISTS:
-            release = lz4_repo.create_git_release(f'v{kmtricks_version}', f'Release v{kmtricks_version}', BUILD_INFO, False, prerelease=False)
+            release = lz4_repo.create_git_release(
+                f'v{kmtricks_version}',
+                f'Release v{kmtricks_version}',
+                BUILD_INFO, False, prerelease=False)
             release.upload_asset(PACKAGE_PATH)
         else:
             print('Package bad version')
