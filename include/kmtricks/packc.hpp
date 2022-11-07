6b2e6a94ea8043c081458f45f9fd8112d84a1303
Teo Lemane <teo.lemane@gmail.com>
2022-11-07 10:07:40 +0100

    fix abs class

--- include/kmtricks/packc.hpp
+++ include/kmtricks/packc.hpp
@@ -13,23 +13,19 @@ namespace km {
 
   std::uint32_t to_n_b(std::uint32_t c, std::uint32_t n)
   {
-    if (c == 1)
-      return 1;
+    if (i)
+    {
+        std::uint64_t r = std::numeric_limits<decltype(i)>::digits - __builtin_clzll(i);
 
-    if (c == 2)
-      return 2;
-
-    auto c_ = std::ceil(std::log2(c));
-    auto p = static_cast<std::uint32_t>(std::pow(2, n)) - 1;
-
-    return c_ > p ? p : c_;
+        return r > ((1 << n) - 1) ? ((1 << n) - 1) : r;
+    }
+    return 0;
   }
 
   void pack_v(const std::vector<std::uint32_t>& vc, std::vector<uint8_t>& v, int w)
   {
     for (std::size_t i = 0, j = 0; i < vc.size(); ++i, j+=w)
     {
-      spdlog::debug("{} -> {}", vc[i], to_n_b(vc[i], w));
       bitpacker::insert(v, j, w, to_n_b(vc[i], w));
     }
   }
