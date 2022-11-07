dd7a6e0224db6a169d591aa52cda59e6118414b4
Teo Lemane <teo.lemane@gmail.com>
2022-11-07 10:27:05 +0100

    fix packc

--- include/kmtricks/packc.hpp
+++ include/kmtricks/packc.hpp
@@ -2,27 +2,28 @@
 
 #include <vector>
 #include <cmath>
+#include <limits>
 #include <bitpacker/bitpacker.hpp>
 
 namespace km {
 
-  std::uint32_t byte_count_pack(std::uint32_t n, std::uint32_t bit)
+  inline std::uint32_t byte_count_pack(std::uint32_t n, std::uint32_t bit)
   {
     return (n * bit + 7) >> 3;
   }
 
-  std::uint32_t to_n_b(std::uint32_t c, std::uint32_t n)
+  inline std::uint32_t to_n_b(std::uint32_t c, std::uint32_t n)
   {
-    if (i)
+    if (c)
     {
-        std::uint64_t r = std::numeric_limits<decltype(i)>::digits - __builtin_clzll(i);
+        std::uint64_t r = std::numeric_limits<decltype(c)>::digits - __builtin_clzll(c);
 
         return r > ((1 << n) - 1) ? ((1 << n) - 1) : r;
     }
     return 0;
   }
 
-  void pack_v(const std::vector<std::uint32_t>& vc, std::vector<uint8_t>& v, int w)
+  inline void pack_v(const std::vector<std::uint32_t>& vc, std::vector<uint8_t>& v, int w)
   {
     for (std::size_t i = 0, j = 0; i < vc.size(); ++i, j+=w)
     {
